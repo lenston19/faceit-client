aaa60ca2f1475dc3fde98c91de6476080ac88b7d
lenston19 <lenston190900@gmail.com>
2022-06-13 05:59:47 +0300

    small changes | create interface

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,1 +1,23 @@
-
+export interface Faculty {
+  name: string;
+}
+export interface Group {
+  name: string;
+  faculty: Faculty;
+}
+export interface Player {
+  csgoStats: CSGOStats;
+  group: Group;
+  firstName?: string;
+  lastName?: string;
+  patronymic?: string;
+  nickname: string;
+}
+export interface CSGOStats {
+  elo: number;
+  kdRatio: number;
+  averageKdRatio: number;
+  averageHs: number;
+  winRate: number;
+  player: number;
+}
