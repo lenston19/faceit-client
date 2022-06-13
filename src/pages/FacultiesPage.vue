ac62b63c136cb8cefc9975fd883ee7a902559b52
lenston19 <lenston190900@gmail.com>
2022-06-13 05:23:28 +0300

    init pages content

---
+++ src/pages/FacultiesPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-container" style="min-heigth: 100vh">
+    <div class="row q-col-gutter-md justify-center q-py-md">
+      <div v-for="i in 10" :key="i" class="col-12 col-md-3 col-lg-4">
+        <q-card>
+          <q-card-section>
+            <div class="text-h5">
+              Факультет ASD
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
